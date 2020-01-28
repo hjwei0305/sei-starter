3085afe04639775113216f13b5020bb41ee69625
liusonglin <songlin.liu@changhong.com>
2020-01-28 08:55:03 +0800

    4.0.1.21 版本，去掉无用注解

--- sei-service-starter/src/main/java/com/changhong/sei/apitemplate/ApiTemplateConfig.java
+++ sei-service-starter/src/main/java/com/changhong/sei/apitemplate/ApiTemplateConfig.java
@@ -54,7 +54,6 @@ public class ApiTemplateConfig {
      * 创建Feign请求拦截器，在发送请求前设置认证的token,各个微服务将token设置到环境变量中来达到通用
      * @return
      */
-    @ConditionalOnBusEnabled
     @Bean
     public FeignBasicAuthRequestInterceptor basicAuthRequestInterceptor() {
         return new FeignBasicAuthRequestInterceptor();
