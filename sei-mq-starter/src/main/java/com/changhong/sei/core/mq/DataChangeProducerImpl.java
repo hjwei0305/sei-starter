80877150f8aedaeba64ca4aa34e59de0ec1f5373
王锦光 <wangjg@changhong.com>
2020-05-11 08:27:50 +0800

    获取系统应用配置的MQ Topic值:VERSION=6.0.2.1

--- sei-mq-starter/src/main/java/com/changhong/sei/core/mq/DataChangeProducerImpl.java
+++ sei-mq-starter/src/main/java/com/changhong/sei/core/mq/DataChangeProducerImpl.java
@@ -1,7 +1,9 @@
 package com.changhong.sei.core.mq;
 
+import com.changhong.sei.core.context.ContextUtil;
 import com.changhong.sei.core.datachange.DataChangeProducer;
 import com.changhong.sei.core.log.LogUtil;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Component;
@@ -16,10 +18,16 @@ import org.springframework.stereotype.Component;
 public class DataChangeProducerImpl implements DataChangeProducer {
     @Autowired
     private KafkaTemplate<String, String> kafkaTemplate;
-    // 约定的MQ Topic
-    private final static String TOPIC = "DataChangeLog";
     // 约定的MQ Topic Key
     private final static String KEY = "DataHistoryRecord";
+
+    /**
+     * 获取系统应用配置的MQ Topic值
+     * @return MQ Topic
+     */
+    private String getTopic() {
+        return ContextUtil.getProperty("sei.datachange.topic");
+    }
 
     /**
      * 发送消息
@@ -28,7 +36,11 @@ public class DataChangeProducerImpl implements DataChangeProducer {
      */
     @Override
     public void send(String message) {
-        kafkaTemplate.send(TOPIC, KEY, message);
+        String topic = getTopic();
+        if (StringUtils.isBlank(topic)) {
+            return;
+        }
+        kafkaTemplate.send(topic, KEY, message);
         LogUtil.bizLog("数据变更记录队列生产者："+message);
     }
 }
