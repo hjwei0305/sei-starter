b76fde13b59386c90b9644d6be5e609957357ae1
Vision.Mac <chao2.ma@changhong.com>
2021-01-19 13:20:01 +0800

    增加junit5支持

--- sei-test-starter/src/main/java/com/changhong/sei/core/test/BaseUnit5Test.java
+++ sei-test-starter/src/main/java/com/changhong/sei/core/test/BaseUnit5Test.java
@@ -3,15 +3,16 @@ package com.changhong.sei.core.test;
 import com.changhong.sei.core.config.properties.mock.MockUserProperties;
 import com.changhong.sei.core.context.mock.MockUser;
 import com.changhong.sei.util.thread.ThreadLocalHolder;
-import org.junit.AfterClass;
-import org.junit.Before;
-import org.junit.BeforeClass;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 /**
  * 实现功能： 单元测试基类
@@ -19,16 +20,17 @@ import org.springframework.test.context.junit4.SpringRunner;
  * @author 马超(Vision.Mac)
  * @version 1.0.00  2020-01-09 15:59
  */
-@RunWith(SpringRunner.class)
+@ExtendWith(SpringExtension.class)
 @SpringBootTest
-public class BaseUnitTest {
-    protected static final Logger LOG = LoggerFactory.getLogger(BaseUnitTest.class);
+public class BaseUnit5Test {
+    protected static final Logger LOG = LoggerFactory.getLogger(BaseUnit5Test.class);
     @Autowired
     public MockUserProperties properties;
     @Autowired
     public MockUser mockUser;
 
-    @BeforeClass
+    @BeforeAll
+    @DisplayName("单元测试初始化")
     public static void setup() {
         // 初始化
         ThreadLocalHolder.begin();
@@ -36,14 +38,16 @@ public class BaseUnitTest {
         LOG.debug("开始进入单元测试.......");
     }
 
-    @Before
+    @BeforeEach
+    @DisplayName("单元测试模拟用户")
     public void mock() {
 //        LOG.debug("当前模拟用户: {}", mockUser.mockUser(properties.getTenantCode(), properties.getAccount()));
         LOG.debug("当前模拟用户: {}", mockUser.mockUser(properties));
     }
 
 
-    @AfterClass
+    @AfterAll
+    @DisplayName("单元测试完成资源释放")
     public static void cleanup() {
         // 释放
         ThreadLocalHolder.end();
